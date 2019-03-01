9c95903dd8881fe84cb80448d054a767c4c042c7
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-03-01 16:40:40 +0900

    npm install --save @gridsome/source-filesystem npm install --save @gridsome/transformer-remark

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,30 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'goal/*.md',
+        typeName: 'BlogPost',
+        route: '/goal/:year/:month/:day',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    }
+  ]
 }
