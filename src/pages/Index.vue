2d9f349aaf6e6e95b56b80b15765d87552c9d8d8
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-03-05 12:06:16 +0900

    詳細ページを削除し、一覧ページに集約

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,17 +3,15 @@
       <h1>今月の目標一覧</h1>
       <div v-for="item in $page.allBlogPost.edges" :key="item.path" class="post">
         <h2>
-          <g-link :to="item.node.path">{{ item.node.title }}</g-link>
+          {{ item.node.title }}
         </h2>
         <h2>
           {{ item.node.auther }}
         </h2>
         <dl>
-          <dt>{{ item.node.date }}</dt><dd>{{ item.node.fields.tags}}</dd>
+          <dt>{{ item.node.date }}</dt>
         </dl>
-        <p>{{ item.node.fields.description }}</p>
-        <g-link :to="item.node.path" class="continue-link">続きを読む ></g-link>
-      </div>
+      <div class="content" v-html="item.node.content" />       </div>
   </Layout>
 </template>
 
@@ -26,11 +24,7 @@
           title
           auther
           date (format: "YYYY年MM月DD日 HH:mm:ss")
-          fields {
-            description
-            tags
-          }
-          path
+          content
         }
       }
     }
