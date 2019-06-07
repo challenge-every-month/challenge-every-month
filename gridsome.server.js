65007693384a960791f78000d7678006414b6eba
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2019-06-08 00:18:19 +0900

    webpackのwhitelistにVuetifyを追加

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,8 +4,17 @@
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
+const nodeExternals = require('webpack-node-externals')
 module.exports = function (api) {
+  api.chainWebpack((config, { isServer }) => {
+    if (isServer) {
+      config.externals([
+        nodeExternals({
+          whitelist: [/^vuetify/]
+        })
+      ])
+    }
+  })
   api.loadSource(store => {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api
   })
