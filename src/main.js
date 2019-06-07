c6bad7da75e6767220e2dbec237686f72082755b
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2019-06-08 00:02:40 +0900

    Vuetifyをimport

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+  })
+  Vue.use(Vuetify)
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
