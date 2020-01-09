da52da9499a85a259a34279808e4bd2d9bf364c4
YOSHIMURA Takuya(吉村 健矢) <takuya.yoshimura@avantcorp.com>
2020-01-09 16:12:08 +0900

    add sitename & siteURL

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,6 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here requires a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
+  siteName: 'challenge-every-mouth',
+  siteUrl: 'https://challenge-every-month.netlify.com/',
   transformers: {
     remark: {
       externalLinksTarget: '_blank',
