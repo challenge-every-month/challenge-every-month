105a964f1f02b0718b8120fbaa652a82ccbbbc5c
YOSHIMURA Takuya(吉村 健矢) <takuya.yoshimura@avantcorp.com>
2020-01-09 14:28:12 +0900

    Headerを分割

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="header">
+  <v-toolbar>
+    <v-toolbar-title class="display-2 font-weight-black">challenge-every-mouth</v-toolbar-title>
+    <v-spacer></v-spacer>
+<!--    <v-toolbar-items>
+    <v-btn>
+        <g-link to="/">目標</g-link>
+    </v-btn>
+    <v-btn>
+        <g-link to="/about">概要</g-link>
+    </v-btn>
+    <v-btn>
+        <g-link to="/howToJoin" class>参加方法</g-link>
+    </v-btn>
+    </v-toolbar-items>-->
+  </v-toolbar>
+ </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,6 @@
 <template>
   <div id="app">
-    <v-toolbar>
-      <v-toolbar-title class="black--text header">challenge-every-mouth</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn>
-          <g-link to="/">目標</g-link>
-        </v-btn>
-        <v-btn>
-          <g-link to="/about">概要</g-link>
-        </v-btn>
-        <v-btn>
-          <g-link to="/howToJoin" class>参加方法</g-link>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    <Header />
     <v-content class="content" style="background-image:url(images/cork.jpg);">
       <slot/>
     </v-content>
@@ -22,15 +8,17 @@
 </template>
 
 <script>
-export default {};
+import Header from '../components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+};
 </script>
 
 <style>
-.header {
-  height: 50px;
-}
 .content {
-  /* margin-top: 25px; */
   background-size: cover;
 }
 </style>
